a76d3df22531eec2ebf6e30e7306586f64a53b1b
Horyniuk Stepan <horyniuk.stepan.dev@gmail.com>
2026-03-18 22:17:44 +0200

    lab4

--- labs/lab (4)/test.m
+++ labs/lab (4)/test.m
@@ -1,44 +1,48 @@
-clear, clc, close all
+clear; clc; close all;
 
-path1 = '/MATLAB Drive/labs/lab1/Cat_November_2010-1a.jpg';
-path2 = '/MATLAB Drive/labs/lab1/imagesbw.jpeg'
-catimg = imread(path1);
-elfantimg = imread(path2);
+% 1-2. Завантаження та підготовка зображення
+f = imread('/MATLAB Drive/labs/lab (4)/Cat_November_2010-1a.jpg');
+if size(f, 3) == 3
+    f = rgb2gray(f);
+end
+f = im2double(f); % Переводимо у double для точності обчислень
 
+% 3-4. Обчислення ДПФ та візуалізація спектра
+F = fft2(f);
+S = abs(F);
+Fc = fftshift(S); % Зсув нульової частоти в центр
+Slog = log(1 + Fc); % Логарифмічний масштаб для наочності
 
-info1 = imfinfo(path1);
-disp('Інформація про 1 зображення:');
-disp(info1);
-info2 = imfinfo(path2);
-disp('Інформація про 2 зображення:');
-disp(info2);
+figure('Name', 'Оригінал та його спектр');
+subplot(1,2,1); imshow(f); title('Вихідне зображення');
+subplot(1,2,2); imshow(Slog, []); title('Спектр (лог. масштаб)');
 
-imwrite(catimg, '/MATLAB Drive/labs/lab1/saved_cat_image.png');
-imwrite(elfantimg, '/MATLAB Drive/labs/lab1/saved_elefant_image.png');
+% Відновлення зображення за спектром (ОДПФ)
+Fdc = ifftshift(Fc); % Зворотний зсув
+f_restored = ifft2(F);
 
-cat_gray = rgb2gray(catimg);
-elfant_gray = rgb2gray(elfantimg);
+figure('Name', 'Відновлення після прямого та зворотного ДПФ');
+imshow(abs(f_restored), []); title('Відновлене зображення');
 
-figure('Name', 'Гістограми та контрастування');
+% 5-8. Фільтрація у частотній області
+[M, N] = size(f);
+sigma = 20; % Параметр розмиття (чим менше, тим вужчий спектр пропускається)
+h = fspecial('gaussian', [M N], sigma); % Фільтр розміром із зображення
 
-subplot(2,2,1); imhist(cat_gray); title('Гістограма: Кіт');
-subplot(2,2,2); imhist(elfant_gray); title('Гістограма: Слон');
+% Частотна характеристика фільтра
+H = fft2(h);
+Hc = fftshift(abs(H));
+Hlog = log(1 + Hc);
 
-cat_adj = imadjust(cat_gray);
-elfant_adj = imadjust(elfant_gray);
+% Застосування фільтра (множення спектрів)
+Filtered_F = F .* H; 
+Filtered_Fc = fftshift(abs(Filtered_F));
+Filtered_Slog = log(1 + Filtered_Fc);
 
-subplot(2,2,3); imshow(cat_adj); title('Кіт: Підвищений контраст');
-subplot(2,2,4); imshow(elfant_adj); title('Слон: Підвищений контраст');
-
-cat_neg = imadjust(catimg, [0 1], [1 0], 1.5);
-elfant_neg_adj = imadjust(elfant_gray, [0 1], [1 0], 1.5);
-
-figure('Name', 'Негативи');
-
-subplot(1,2,1); 
-imshow(cat_neg); 
-title('Негатив кота');
-
-subplot(1,2,2); 
-imshow(elfant_neg_adj); 
-title('Негатив слона');
+% Зворотне перетворення для отримання відфільтрованого зображення
+f_filtered = abs(ifft2(Filtered_F));
+f_filtered = fftshift(f_filtered);
+figure('Name', 'Фільтрація у частотній області');
+subplot(1,3,1); imshow(Hlog, []); title('АЧХ фільтра');
+subplot(1,3,2); imshow(Filtered_Slog, []); title('Спектр після фільтрації');
+subplot(1,3,3); imshow(f_filtered, []); title('Відфільтроване зображення');
